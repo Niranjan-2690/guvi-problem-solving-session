2254fe4b5d0109ec44c7307616b2b6b2a7267eea
Niranjan-2690 <niranjan.niran4@gmail.com>
2025-04-02 23:39:22 +0530

    Updated

---
+++ session-3.js
@@ -0,0 +1,28 @@
+// ? Day 3: Strings & String Manipulation
+// * Session Focus: Basic string operations, string traversal, and manipulation techniques.
+// ? Session Practice Questions:
+
+// 1. Reverse a string.
+
+// 2. Count vowels and consonants in a string.
+
+// 3. Convert a string to lowercase / uppercase.
+
+// 4. Find the longest word in a sentence.
+
+// 5. Check if a string is a palindrome.
+
+// 6. Remove duplicates from a string.
+
+// 7. Find all substrings of a given string.
+
+// 8. Post - Session Practice Questions:
+// 9. Concatenate two strings.
+// 10. Find the frequency of each character in a string.
+// 11. Replace spaces in a string with % 20.
+// 12. Check if a string is an anagram of another.
+// 13. Count the number of words in a sentence.
+// 14. Find the first non - repeating character in a string.
+// 15. Remove all vowels from a string.
+// 16. Find the shortest word in a sentence.
+// 17. Count occurrences of a substring within a string.
